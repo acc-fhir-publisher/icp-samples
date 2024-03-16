9eface72334cb1b5eb227054e0109ed57c2785c7
besserg <besserg@gmail.com>
2024-03-16 23:59:59 +1300

    Add FHIR specification and terminology services
    The code adds FHIR specification and terminology services. It sets up a FHIR package source, a cached resolver to resolve resources, and a local terminology service. A commented-out line for a validator is also added, suggesting future implementation.

---
+++ dotnet/Program.cs
@@ -0,0 +1,11 @@
+﻿using Firely.Fhir.Packages;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Specification.Source;
+using Hl7.Fhir.Specification.Terminology;
+
+var packageResolver = new FhirPackageSource(ModelInfo.ModelInspector, new string[] {""});
+var resourceResolver = new CachedResolver(packageResolver);
+var terminologyService = new LocalTerminologyService(resourceResolver);
+
+
+// var validator = new Validator(resourceResolver, terminologyService);
