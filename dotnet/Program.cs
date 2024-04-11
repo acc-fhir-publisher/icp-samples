63834578a2f4253879dca9dcda0fc238cc0a398a
besserg <besserg@gmail.com>
2024-04-11 19:15:17 +1200

    Update FHIR package resolver and remove unused extensions
    Updated the FHIR package resolver in the .NET application to use actual path instead of an empty string. Also removed the unused extensions from icp-case-create.json. In addition to this, added JSON file reading and resource validation in the program, increasing the overall functionality.

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,10 +1,27 @@
 ﻿using Firely.Fhir.Packages;
 using Firely.Fhir.Validation;
 using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
 using Hl7.Fhir.Specification.Source;
 using Hl7.Fhir.Specification.Terminology;
+using Hl7.Fhir.Support;
 
-var packageResolver = new FhirPackageSource(ModelInfo.ModelInspector, new string[] {""});
+var packageResolver = new FhirPackageSource(ModelInfo.ModelInspector, [
+    @"..\..\..\..\java\src\main\resources\package.r4.tgz"
+]);
 var resourceResolver = new CachedResolver(packageResolver);
 var terminologyService = new LocalTerminologyService(resourceResolver);
 var validator = new Validator(resourceResolver, terminologyService);
+
+var jsonFilePath = @"..\..\..\..\java\src\main\resources\icp-case-create.json";
+var json = File.ReadAllText(jsonFilePath);
+
+var parser = new FhirJsonParser();
+var resource = parser.Parse<EpisodeOfCare>(json);
+
+var result = validator.Validate(resource, "http://hl7.org.nz/fhir/StructureDefinition/acc-icp-case-create");
+
+foreach (var validationError in result.ListErrors())
+{
+    Console.WriteLine(validationError.Details.Text);
+}
